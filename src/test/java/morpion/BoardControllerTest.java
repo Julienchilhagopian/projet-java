7d8e0a9dcbd5c100885c7f5ec9e01a7c28372e2f
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-01-24 12:29:31 +0100

    boardcontroller test

--- src/test/java/morpion/BoardControllerTest.java
+++ src/test/java/morpion/BoardControllerTest.java
@@ -26,7 +26,7 @@ public class BoardControllerTest {
         assertFalse(noTracePoint.isActive());
         assertEquals(0, noTracePoint.getTraces().size());
 
-        // Point éligible pour un trace verticale
+        // Point éligible pour un trace verticale (6:2)
         Point traceEligiblePoint = controller.getBoardModel().getPoints().get(98);
         JButton buttonEligible = view.getButton(traceEligiblePoint);
         buttonEligible.doClick();
@@ -39,6 +39,44 @@ public class BoardControllerTest {
         assertEquals(1, controller.getBoardModel().getPoints().get(100).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(101).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(102).getTraces().size());
+        // voisin suivant en dehors de la trace.
         assertEquals(0, controller.getBoardModel().getPoints().get(103).getTraces().size());
+
+
+        // Point éligible pour un trace horizontale (10:12)
+        traceEligiblePoint = controller.getBoardModel().getPoints().get(172);
+        buttonEligible = view.getButton(traceEligiblePoint);
+        buttonEligible.doClick();
+
+        assertTrue(traceEligiblePoint.isActive());
+        assertEquals(1, traceEligiblePoint.getTraces().size());
+        // Chaque point de la lignée doit posséder une trace.
+        assertEquals(1, controller.getBoardModel().getPoints().get(156).getTraces().size());
+        assertEquals(1, controller.getBoardModel().getPoints().get(140).getTraces().size());
+        assertEquals(1, controller.getBoardModel().getPoints().get(124).getTraces().size());
+        assertEquals(1, controller.getBoardModel().getPoints().get(108).getTraces().size());
+        // voisin suivant en dehors de la trace.
+        assertEquals(0, controller.getBoardModel().getPoints().get(92).getTraces().size());
+
+
+        // Point éligible pour un trace diagonale (5:5)
+        traceEligiblePoint = controller.getBoardModel().getPoints().get(85);
+        buttonEligible = view.getButton(traceEligiblePoint);
+        buttonEligible.doClick();
+
+        assertTrue(traceEligiblePoint.isActive());
+        assertEquals(1, traceEligiblePoint.getTraces().size());
+        // Chaque point de la lignée doit posséder une trace.
+        assertEquals(1, controller.getBoardModel().getPoints().get(70).getTraces().size());
+        assertEquals(1, controller.getBoardModel().getPoints().get(55).getTraces().size());
+
+        // On a deja tracé sur le point 100 dans ce test. Il doit posséder 2 traces.
+        assertEquals(2, controller.getBoardModel().getPoints().get(100).getTraces().size());
+        assertEquals(1, controller.getBoardModel().getPoints().get(115).getTraces().size());
+
+        // voisin suivant en dehors de la trace.
+        assertEquals(0, controller.getBoardModel().getPoints().get(145).getTraces().size());
+
+
     }
 }
