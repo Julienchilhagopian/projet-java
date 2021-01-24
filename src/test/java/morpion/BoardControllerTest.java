d110ee231344b9543cc0967cd85f1918b7c4d507
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-01-24 15:38:05 +0100

    english + format

--- src/test/java/morpion/BoardControllerTest.java
+++ src/test/java/morpion/BoardControllerTest.java
@@ -27,11 +27,11 @@ public class BoardControllerTest {
         JButton button = view.getButton(noTracePoint);
         button.doClick();
 
-        // Il n'est pas possible de tracer sur ce point.
+        // This point cannot be traced.
         assertFalse(noTracePoint.isActive());
         assertEquals(0, noTracePoint.getTraces().size());
 
-        // Point éligible pour un trace verticale (6:2)
+        // This point is eligible for vertical trace (6:2)
         Point traceEligiblePoint = controller.getBoardModel().getPoints().get(98);
         JButton buttonEligible = view.getButton(traceEligiblePoint);
         buttonEligible.doClick();
@@ -39,47 +39,47 @@ public class BoardControllerTest {
         assertTrue(traceEligiblePoint.isActive());
         assertEquals(1, traceEligiblePoint.getTraces().size());
 
-        // Chaque point de la lignée doit posséder une trace.
+        // Each point must own a trace.
         assertEquals(1, controller.getBoardModel().getPoints().get(99).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(100).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(101).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(102).getTraces().size());
-        // Voisin suivant en dehors de la trace.
+        // Next neighbor outside the trace
         assertEquals(0, controller.getBoardModel().getPoints().get(103).getTraces().size());
 
 
-        // Point éligible pour un trace horizontale (10:12)
+        // This point is eligible for horizontal trace  (10:12)
         traceEligiblePoint = controller.getBoardModel().getPoints().get(172);
         buttonEligible = view.getButton(traceEligiblePoint);
         buttonEligible.doClick();
 
         assertTrue(traceEligiblePoint.isActive());
         assertEquals(1, traceEligiblePoint.getTraces().size());
-        // Chaque point de la lignée doit posséder une trace.
+        // Each point must own a trace.
         assertEquals(1, controller.getBoardModel().getPoints().get(156).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(140).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(124).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(108).getTraces().size());
-        // voisin suivant en dehors de la trace.
+        // Next neighbor outside the trace
         assertEquals(0, controller.getBoardModel().getPoints().get(92).getTraces().size());
 
 
-        // Point éligible pour un trace diagonale (5:5)
+        // This point is eligible for diagonal trace (5:5)
         traceEligiblePoint = controller.getBoardModel().getPoints().get(85);
         buttonEligible = view.getButton(traceEligiblePoint);
         buttonEligible.doClick();
 
         assertTrue(traceEligiblePoint.isActive());
         assertEquals(1, traceEligiblePoint.getTraces().size());
-        // Chaque point de la lignée doit posséder une trace.
+        // Each point must own a trace.
         assertEquals(1, controller.getBoardModel().getPoints().get(70).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(55).getTraces().size());
 
-        // On a deja tracé sur le point 100 dans ce test. Il doit posséder 2 traces.
+        // We already pinned point 100. It must own 2 traces.
         assertEquals(2, controller.getBoardModel().getPoints().get(100).getTraces().size());
         assertEquals(1, controller.getBoardModel().getPoints().get(115).getTraces().size());
 
-        // voisin suivant en dehors de la trace.
+        // Next neighbor outside the trace
         assertEquals(0, controller.getBoardModel().getPoints().get(145).getTraces().size());
 
     }
@@ -90,21 +90,21 @@ public class BoardControllerTest {
         // launch du mode 5T
         view.getButton5T().doClick();
 
-        // Trace horizontale sur le point (8:9)
+        // This point is eligible for diagonal trace  (8:9)
         Point traceEligiblePoint = controller.getBoardModel().getPoints().get(137);
         JButton buttonEligible = view.getButton(traceEligiblePoint);
         buttonEligible.doClick();
 
-        // Recuperation du point adjacent 5T (7:9)
+        // Retrieving adjacent point 5T (7:9)
         Point eligiblePoint = controller.getBoardModel().getPoints().get(121);
 
         Trace traceHorizontal = new Trace5T("Horizontal");
 
-        // Les points adjacents sont autorisés en 5T.
+        // Adjacent points are allowed in 5T.
         assertTrue(eligiblePoint.isEligible(traceHorizontal));
 
 
-        // Trace verticale sur le point (6:8)
+        // This point is eligible for vertical trace  (6:8)
         traceEligiblePoint = controller.getBoardModel().getPoints().get(104);
         buttonEligible = view.getButton(traceEligiblePoint);
         buttonEligible.doClick();
@@ -114,6 +114,6 @@ public class BoardControllerTest {
         Trace traceVerticale = new Trace5T("Vertical");
 
         assertTrue(eligiblePoint.isEligible(traceVerticale));
-        
+
     }
 }
--- src/test/java/morpion/BoardModelTest.java
+++ src/test/java/morpion/BoardModelTest.java
@@ -11,77 +11,76 @@ import static org.junit.jupiter.api.Assertions.*;
  * Please enter a pseudo each time a test is launched.
  */
 public class BoardModelTest {
-	private Board board = Board.withClassicBoard();
+    private Board board = Board.withClassicBoard();
 
-	@Test
-	void testModel() {
+    @Test
+    void testModel() {
 
-		assertEquals(256, board.getPoints().size());
+        assertEquals(256, board.getPoints().size());
 
-		int nbActive = board.countActive();
-		board.setActive(board.getPoints().get(3));
+        int nbActive = board.countActive();
+        board.setActive(board.getPoints().get(3));
 
-		assertTrue(board.getPoints().get(3).isActive());
-		assertEquals(nbActive+1, board.countActive());
+        assertTrue(board.getPoints().get(3).isActive());
+        assertEquals(nbActive + 1, board.countActive());
 
-		assertEquals(0, board.getPoints().get(87).getNeighbors().size());
-		board.updateVoisins();
-		assertEquals(3, board.getPoints().get(87).getNeighbors().size());
+        assertEquals(0, board.getPoints().get(87).getNeighbors().size());
+        board.updateVoisins();
+        assertEquals(3, board.getPoints().get(87).getNeighbors().size());
 
-		// Le point 3 est actif donc le point 2 doit posséder 1 voisin
-		assertEquals(1, board.getPoints().get(2).getNeighbors().size());
-
-	}
-
-
-	@Test
-	void testModelPoint() {
-		Trace trace = new Trace("Vertical");
-
-		// Ce point ne possède pas de trace il est éligible à en avoir une.
-		Point pointTest = board.getPoints().get(1);
-		assertTrue(pointTest.isEligible(trace));
-
-		pointTest.addTraces(trace);
-
-		// Après l'ajout d'une trace vertical le point n'est plus éligible.
-		assertFalse(pointTest.isEligible(trace));
-
-		board.updateVoisins();
-		// Target du point 3:5 pour test de descente verticale
-		Point startPoint = board.getPoints().get(53);
-		int counter = 1;
-
-		while(startPoint.getDownNeighbor().isPresent()) {
-			startPoint = startPoint.getDownNeighbor().get();
-			counter++;
-		}
-
-		// A partir du point 3:5 inclus on peut descendre 3 fois.
-		assertEquals(5, counter);
-
-		// Target du point 8:6 pour test de déplacement horizontal
-		startPoint = board.getPoints().get(134);
-		counter = 1;
-
-		while(startPoint.getRightNeighbor().isPresent()) {
-			startPoint = startPoint.getRightNeighbor().get();
-			counter++;
-		}
-
-		assertEquals(5, counter);
+        // Point 3 is active so point 2 must own a neighbor
+        assertEquals(1, board.getPoints().get(2).getNeighbors().size());
+    }
 
 
-		// Target du point 10:10 pour test de déplacement diagonal
-		startPoint = board.getPoints().get(170);
-		counter = 1;
+    @Test
+    void testModelPoint() {
+        Trace trace = new Trace("Vertical");
 
-		while(startPoint.getDownLeftNeighbor().isPresent()) {
-			startPoint = startPoint.getDownLeftNeighbor().get();
-			counter++;
-		}
+        // This point does not own a trace, he is eligible for one.
+        Point pointTest = board.getPoints().get(1);
+        assertTrue(pointTest.isEligible(trace));
 
-		// on doit pouvoir visiter 3 points en diagonal descendante depuis ce point.
-		assertEquals(3, counter);
-	}
+        pointTest.addTraces(trace);
+
+        // After adding a vertical trace, the point is not eligible anymore.
+        assertFalse(pointTest.isEligible(trace));
+
+        board.updateVoisins();
+        // Targeting point 3:5 to test a descending vertical.
+        Point startPoint = board.getPoints().get(53);
+        int counter = 1;
+
+        while (startPoint.getDownNeighbor().isPresent()) {
+            startPoint = startPoint.getDownNeighbor().get();
+            counter++;
+        }
+
+        // From point 3:5 included we can go down 3 times.
+        assertEquals(5, counter);
+
+        // Targeting point 8:6 to test horizontal move.
+        startPoint = board.getPoints().get(134);
+        counter = 1;
+
+        while (startPoint.getRightNeighbor().isPresent()) {
+            startPoint = startPoint.getRightNeighbor().get();
+            counter++;
+        }
+
+        assertEquals(5, counter);
+
+
+        // Targeting point 10:10 to test diagonal move.
+        startPoint = board.getPoints().get(170);
+        counter = 1;
+
+        while (startPoint.getDownLeftNeighbor().isPresent()) {
+            startPoint = startPoint.getDownLeftNeighbor().get();
+            counter++;
+        }
+
+        // From point  10:10 included we can visit 3 point going down in diagonal
+        assertEquals(3, counter);
+    }
 }
